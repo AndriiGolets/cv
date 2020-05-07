59077396879e3ec7f145b393f784bb089716fa74
AndriiGolets <a306013@gmail.com>
2020-05-07 18:41:26 +0300

    docker support

--- src/main/java/name/golets/resume/AppRunner.java
+++ src/main/java/name/golets/resume/AppRunner.java
@@ -2,8 +2,10 @@ package name.golets.resume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "name.golets.resume")
 public class AppRunner {
 
     public static void main(String[] args) {
